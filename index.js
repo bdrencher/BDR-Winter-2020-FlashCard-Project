b4be163941ec1dba6ba2c6b365be1f4705381504
bdrencher <bdrencher@gmail.com>
2020-03-11 18:35:11 -0400

    running a test of the postgresql database

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const { Pool } = require('pg');
 const app = express();
+
 const connectionString = process.env.DATABASE_URL || "postgres://vbwooxkbxkjvbz:f8a15944d000f50dbcc8c2d1370bbd56d31533e2dd90ddfb61381b9dc2b84e8b@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d9mfcl4vbslvf5?ssl=true";
-const port = process.env.PORT || 8080;
+const { Pool } = require('pg');
 const clientPool = new Pool({connectionString: connectionString});
 
+const port = process.env.PORT || 8080;
 app.listen(port);
+
+//test query
+const query = "INSERT INTO name VALUES ('testName')";
+clientPool.query(query, function (error, response) {
+    if (error)
+    {
+        console.log(error);
+    }
+
+    console.log("after error if block");
+});
 
 app.use(express.static(__dirname + '/public'));
 
