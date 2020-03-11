94058bc4ad02fe44dbdcb4bd5d888dba150b0fdc
bdrencher <bdrencher@gmail.com>
2020-03-11 18:15:41 -0400

    fixed pool variable assignment so the Pool constructor will work

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const pool = require('pg');
+const { Pool } = require('pg');
 const app = express();
 const connectionString = process.env.DATABASE_URL || "postgres://vbwooxkbxkjvbz:f8a15944d000f50dbcc8c2d1370bbd56d31533e2dd90ddfb61381b9dc2b84e8b@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d9mfcl4vbslvf5?ssl=true";
 const port = process.env.PORT || 8080;
-const clientPool = new pool({connectionString: connectionString});
+const clientPool = new Pool({connectionString: connectionString});
 
 app.listen(port);
 
