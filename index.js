78366201e87b58e6aa6aabffda75c0629c7851cf
bdrencher <bdrencher@gmail.com>
2020-03-11 17:14:23 -0400

    had two pool variables, one is renamed clientPool

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { pool } = require('pg');
+const pool = require('pg');
 const app = express();
 const connectionString = process.env.DATABASE_URL || "postgres://vbwooxkbxkjvbz:f8a15944d000f50dbcc8c2d1370bbd56d31533e2dd90ddfb61381b9dc2b84e8b@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d9mfcl4vbslvf5?ssl=true";
 const port = process.env.PORT || 8080;
-const pool = new pool({connectionString: connectionString});
+clientPool = new pool({connectionString: connectionString});
 
 app.listen(port);
 
